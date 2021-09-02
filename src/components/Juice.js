8a984d6516c4b8afeca2372b11b88d6d160a740e
moradalkhatib <muradalkhateb@icloud.com>
2021-09-02 16:30:49 +0300

    last touch

--- src/components/Juice.js
+++ src/components/Juice.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Card ,Button,Row,Col} from "react-bootstrap";
+import { Card, Button, Row, Col, Alert } from "react-bootstrap";
 import { withAuth0 } from "@auth0/auth0-react";
+import Swal from "sweetalert2";
 class Juice extends Component {
   constructor() {
     super();
@@ -11,37 +12,53 @@ class Juice extends Component {
   }
   componentDidMount() {
     axios.get(`https://juicepine.herokuapp.com/juice`).then((response) => {
-      console.log(response.data);
       this.setState({
         allJuice: response.data,
       });
     });
   }
-  addFavorite(item){
-      let bodyJuice={
-        email:this.props.auth0.user.email,
-        name:item.name,
-        url:item.image,
-        des:item.description
-      }
-      axios.post(`https://juicepine.herokuapp.com/add-fav`,bodyJuice).then(response=>{
-          console.log(response.data);
-      })
+  addFavorite(item) {
+    let bodyJuice = {
+      email: this.props.auth0.user.email,
+      name: item.name,
+      url: item.image,
+      des: item.description,
+    };
+    axios
+      .post(`https://juicepine.herokuapp.com/add-fav`, bodyJuice)
+      .then((response) => {
+        Swal.fire("Added To Favorite Successfully");
+      });
   }
 
   render() {
     return (
       <Row>
-        {this.state.allJuice.map((item,ind) => {
+        {this.props.auth0.isAuthenticated && (
+          <Alert style={{ textAlign: "center" }} variant="success">
+            Welcome {this.props.auth0.user.name}
+          </Alert>
+        )}
+        {this.state.allJuice.map((item, ind) => {
           return (
-           <Col lg={4} key={ind}> <Card style={{ width: "18rem" }}>
-           <Card.Img variant="top" src={item.image} />
-           <Card.Body>
-             <Card.Title>{item.name}</Card.Title>
-             <Card.Title>{item.description} </Card.Title>
-             {this.props.auth0.isAuthenticated &&<Button variant="primary" onClick={()=>this.addFavorite(item)}>Add To Favorite</Button> }
-           </Card.Body>
-         </Card></Col>
+            <Col lg={4} key={ind}>
+              {" "}
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={item.image} />
+                <Card.Body>
+                  <Card.Title>{item.name}</Card.Title>
+                  <Card.Title>{item.description} </Card.Title>
+                  {this.props.auth0.isAuthenticated && (
+                    <Button
+                      variant="primary"
+                      onClick={() => this.addFavorite(item)}
+                    >
+                      Add To Favorite
+                    </Button>
+                  )}
+                </Card.Body>
+              </Card>
+            </Col>
           );
         })}
       </Row>
